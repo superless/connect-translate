905b0de42ab96945bb3c192407e98309acb48398
alejandro.iglesias <alejandro.iglesias@trifenix.io>
2020-12-18 00:03:56 -0300

    resuelto problema de namespace

--- GoogleTranslator.cs
+++ GoogleTranslator.cs
@@ -1,7 +1,7 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Net.Http;
-using trifenix.connect.interfaces.upload.translate;
+using trifenix.connect.interfaces.translate;
 
 namespace trifenix.connect.translate
 {
