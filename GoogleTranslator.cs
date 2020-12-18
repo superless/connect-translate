ad5baefa1263afc8cce369ebdec6e06b9741daa3
alejandro.iglesias <alejandro.iglesias@trifenix.io>
2020-12-17 23:56:18 -0300

    listo pa deploy

---
+++ GoogleTranslator.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using trifenix.connect.interfaces.upload.translate;
+
+namespace trifenix.connect.translate
+{
+
+    /// <summary>
+    /// Traduce un texto del inglés al español
+    /// </summary>
+    public class GoogleTranslator : IGoogleTranslator {
+        public string TranslateText(string textToTranslate) {
+            HttpClient httpClient = new HttpClient();
+            string langFrom = "en";
+            string langTo = "es";
+            string url = "https://translate.googleapis.com/translate_a/single?client=gtx&dt=t&sl=" + langFrom + "&tl=" + langTo + "&q=" + textToTranslate;
+            string result = httpClient.GetStringAsync(url).Result;
+            httpClient.Dispose();
+            dynamic jsonData = JsonConvert.DeserializeObject(result);
+            JArray jArray = jsonData[0];
+            var enumerator = jArray.GetEnumerator();
+            enumerator.MoveNext();
+            string translation = (string)enumerator.Current[0];
+            return translation;
+        }
+
+    }
+
+}
